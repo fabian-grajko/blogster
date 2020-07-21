5c8ca7bcc05cf5a04959911454ba4fb84da5ff82
Fabian <kos.fabian@gmail.com>
2020-07-21 18:34:46 +0200

    added file upload to react app

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb+srv://node-blog:BNolftB269NYNegS@cluster0.lxjzk.mongodb.net/blog-dev?retryWrites=true&w=majority',
-  cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379',
-};
